9fe48ca435f6c3f1acec9ebffeae9b999dfda74d
ThreeTen22 <gtumen@gmail.com>
2017-04-18 01:10:46 -0500

    mod completed
    woo

--- scripts/actions/npc/tonauac.lua
+++ scripts/actions/npc/tonauac.lua
@@ -12,60 +12,57 @@ function blessPlayer(args, output)
   local rotation = BData:getNumber(args.rotation)
   local seed = math.floor(os.time() / rotation)
 
-  local containerId = world.entityQuery(entity.position(), 15, { includedTypes = {"object"},callScript = "config.getParameter",callScriptArgs = {"objectType"}, callScriptResult = "container"})
-  containerId = containerId[1]
-  local items = world.containerItems(containerId)
-  for i = 0, jsize(items) do
-    local item = items[i]
-    if item.parameters and item.parameters.timeToRot then
-      local effect, duration = getEffects(item.name)
-    end
-  end
-
-
-
-
   math.randomseed(seed)
   local blessing = blessings[math.random(1,#blessings)]
   world.sendEntityMessage(entityId, "applyStatusEffect", blessing, duration, entity.id());
+  addFoodEffects(entityId)
   return true
 end
 
-function getEffects(itemName)
-  local effect = nil
-  local duration = 0
+function addFoodEffects(entityId)
+  local containerId = world.entityQuery(entity.position(), 15, {includedTypes = {"object"},callScript = "config.getParameter",callScriptArgs = {"objectType"}, callScriptResult = "container"})
+  containerId = containerId[1]
 
+  local items = world.containerItems(containerId) or {}
+  local effects = {}
 
-  return effect, duration
-end
-
-
-function dLog(item, prefix)
-  if not prefix then prefix = "" end
-  if type(item) ~= "string" then
-    sb.logInfo("%s",prefix.."  "..dOut(item))
-  else 
-    sb.logInfo("%s",prefix.."  "..dOut(item))
+  for i,item in ipairs(items) do
+    if item then
+      if item.parameters and item.parameters.timeToRot then
+        addEffects(item.name, item.count, effects)
+      end
+    end
   end
-end
-
-function dOut(input)
-  if not input then input = "" end
-  return sb.print(input)
-end
-
-function dLogJson(input, prefix, clean)
-  local str = "\n"
-  if clean == true then clean = 1 else clean = 0 end
-  if prefix ~= "true" and prefix ~= "false" and prefix then
-    str = prefix..str
+  for k,v in pairs(effects) do
+    world.sendEntityMessage(entityId, "applyStatusEffect", k, v, entity.id());
   end
-   local info = sb.printJson(input, clean)
-   sb.logInfo("%s", str..info)
+  world.containerTakeAll(containerId)
 end
 
-function dPrintJson(input)
-  local info = sb.printJson(input,1)
-  sb.logInfo("%s",info)
-  return info
+
+--[[
+effects parameter :
+  {
+    effectName: effectDuration
+    effectName2: effectDuration
+  }
+  effects do not last more than 24 hours.  
+  effects of the same type provided by other foods will overwrite the duration.
+--]]
+
+
+function addEffects(itemName, count, effects)
+  count = count or 1
+  count = math.max(count, 1)
+  local itemConfig = root.itemConfig(itemName)
+  local effectConfig = itemConfig.config.effects or {}
+  for i,v in pairs(effectConfig) do
+    for k,v in pairs(v) do
+      if effects[v.effect] then
+       effects[v.effect] = math.min(effects[v.effect] + v.duration*count, 86400)
+      else
+        effects[v.effect] = math.min(v.duration*count, 86400)
+      end
+    end
+  end
 end
