ab11994ab6b19a9f9d0307fb784aaef417001aec
ThreeTen22 <gtumen@gmail.com>
2018-06-08 01:13:50 -0500

    fixed for 1.3.3

--- scripts/actions/npc/tonauac.lua
+++ scripts/actions/npc/tonauac.lua
@@ -1,26 +1,35 @@
 function blessPlayer(args, output)
-  args = parseArgs(args, {
-    entity = nil,
-    blessings = {},
-    duration = 1800,
-    rotation = 86400
-  })
-
-  local entityId = BData:getEntity(args.entity)
-  local blessings = BData:getList(args.blessings)
-  local duration = BData:getNumber(args.duration)
-  local rotation = BData:getNumber(args.rotation)
-  local seed = math.floor(os.time() / rotation)
-
+  local seed = math.floor(os.time() / args.rotation)
   math.randomseed(seed)
-  local blessing = blessings[math.random(1,#blessings)]
-  world.sendEntityMessage(entityId, "applyStatusEffect", blessing, duration, entity.id());
-  addFoodEffects(entityId)
+  
+  local blessing = args.blessings[math.random(1,#args.blessings)]
+  world.sendEntityMessage(args.entity, "applyStatusEffect", blessing, args.duration, entity.id());
+  addFoodEffects(args.entity)
   return true
 end
 
+local isTableEmpty = function(tbl)
+  for _,_ in pairs(tbl) do
+    return false
+  end
+  return true
+end
+
+local copyTable = function(v)
+  if type(v) ~= "table" then
+    return v
+  else
+    local c = {}
+    for k,v in pairs(v) do
+      c[k] = copy(v)
+    end
+    setmetatable(c, getmetatable(v))
+    return c
+  end
+end
+
 function addFoodEffects(entityId)
-  local containerId = world.entityQuery(entity.position(), 15, {includedTypes = {"object"},callScript = "config.getParameter",callScriptArgs = {"objectType"}, callScriptResult = "container"})
+  local containerId = world.objectQuery(entity.position(), 15, {callScript = "config.getParameter",callScriptArgs = {"objectType"}, callScriptResult = "container"})
   if containerId then
     containerId = containerId[1]
   else
@@ -28,45 +37,37 @@ function addFoodEffects(entityId)
   end
 
   local items = world.containerItems(containerId) or {}
-  local effects = {}
-
-  for i,item in ipairs(items) do
+  local effectMap = {}
+  if not isTableEmpty(items) then
+    require "/scripts/augments/item.lua"
+  end
+  for _,item in pairs(items) do
     if item then
       if item.parameters and item.parameters.timeToRot then
-        addEffects(item.name, item.count, effects)
+        addEffects(Item.new(item), item.count, effectMap)
       end
     end
   end
-  for k,v in pairs(effects) do
-    world.sendEntityMessage(entityId, "applyStatusEffect", k, v, entity.id());
+  for _,v in pairs(effectMap) do 
+      world.sendEntityMessage(entityId, "applyStatusEffect", v.effect, v.duration, entity.id())
   end
+
   world.containerTakeAll(containerId)
 end
 
 
---[[
-effects parameter :
-  {
-    effectName: effectDuration
-    effectName2: effectDuration
-  }
-  effects do not last more than 24 hours.  
-  effects of the same type provided by other foods will overwrite the duration.
---]]
-
-
-function addEffects(itemName, count, effects)
+function addEffects(item, count, blessingEffects)
   count = count or 1
   count = math.max(count, 1)
-  local itemConfig = root.itemConfig(itemName)
-  local effectConfig = itemConfig.config.effects or {}
-  for i,v in pairs(effectConfig) do
-    for k,v in pairs(v) do
-      if effects[v.effect] then
-       effects[v.effect] = math.min(effects[v.effect] + v.duration*count, 86400)
-      else
-        effects[v.effect] = math.min(v.duration*count, 86400)
-      end
+
+  local foodEffects = item:instanceValue("effects.0", {})
+  
+  for i,effectObj in ipairs(foodEffects) do
+    local outputEffect = blessingEffects[effectObj.effect] or copyTable(effectObj)
+    if outputEffect.duration then
+      outputEffect.duration = math.min(outputEffect.duration + effectObj.duration*count, 86400)
     end
+    blessingEffects[outputEffect.effect] = copyTable(outputEffect)
   end
+  sb.logInfo("\n%s", sb.printJson(blessingEffects, 1))
 end
